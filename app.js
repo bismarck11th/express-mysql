4445dec6c8b2b93f1e24e9141195a335771929e8
kazuki827 <xm.kazuki827@gmail.com>
2021-11-28 07:26:49 +0900

    ejs setup

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const express = require('express');
 require('dotenv').config();
+
 const PORT = process.env.PORT;
-console.log(PORT);
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/index.js'));
+
+app.listen(PORT, () => {
+  console.log(`Application listening at ${PORT}`);
+});
