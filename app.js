c31ea1d417d895b3556f5bfb19ccd70911dd73cb
kazuki827 <xm.kazuki827@gmail.com>
2021-11-28 10:41:53 +0900

    log setting

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 require('dotenv').config();
 const path = require('path');
+const logger = require('./lib/log/logger.js');
+const accesslogger = require('./lib/log/accesslogger');
+const applicationLogger = require('./lib/log/applicationlogger');
 const express = require('express');
 const favicon = require('serve-favicon');
 const PORT = process.env.PORT;
 
-// Express settings
 const app = express();
+
+// Express settings
 app.set('view engine', 'ejs');
+app.disable('x-powered-by');
 
 // Static resource routing
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
+// Set access log
+app.use(accesslogger());
+
 // Dynamic resource routing
 app.use('/', require('./routes/index.js'));
 
+// Set application logger
+app.set(applicationLogger());
+
 // Execute web application
 app.listen(PORT, () => {
-  console.log(`Application listening at ${PORT}`);
+  logger.application.info(`Application listening at ${PORT}`);
 });
